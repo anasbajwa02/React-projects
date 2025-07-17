5462da628c67bd4cefd0ea8ba5a34761c321e056
anasbajwa02 <anas03154297874@gmail.com>
2025-07-17 19:32:27 +0500

    complete about section

--- portfolio/src/components/About/About.jsx
+++ portfolio/src/components/About/About.jsx
@@ -15,6 +15,77 @@ const About = () => {
                     <a href="cv.pdf" download="cv.pdf" target="_blank">Download-CV </a>
                 </button>
                 <h3>Technical Expertise</h3>
+
+                <div className="experties">
+
+                  <div className='exp-single'>
+                    <h2>Programing Languages</h2>
+                    <ul>
+                      <li>JavaScript</li>
+                      <li>C++</li>
+                    </ul>
+                  </div>
+
+                   <div className='exp-single'>
+                    <h2>Frontend Development</h2>
+                    <ul>
+                      <li>React</li>
+                      <li>Tailwind</li>
+                      <li>BootStrap</li>
+                      <li>HTML5</li>
+                      <li>CSS3</li>
+                      <li>Redux</li>
+                      <li>Redux ToolKit</li>
+
+
+
+                    </ul>
+                  </div>
+
+                   <div className='exp-single'>
+                    <h2>UI/UX Design</h2>
+                    <ul>
+                      <li>Figma</li>
+                      <li>Adobe Illustrator</li>
+                      <li>Adobe PhotoShop</li>
+
+                    </ul>
+                  </div>
+
+                   <div className='exp-single'>
+                    <h2>Tools & Technologies</h2>
+                    <ul>
+                      <li>Git</li>
+                      <li>Git Hub</li>
+                      <li>Agile (Scrum,  Kanban)</li>
+
+                    </ul>
+                  </div>
+
+                   <div className='exp-single'>
+                    <h2>Soft Skills</h2>
+                    <ul>
+                      <li>Presentation</li>
+                      <li>Communication</li>
+                      <li>Professional Demeanor</li>
+                      <li>Leadership</li>
+                      <li>Mentorship</li>
+
+                    </ul>
+                  </div>
+
+                  
+                   <div className='exp-single'>
+                    <h2>Extra Skills</h2>
+                    <ul>
+                      <li>Hard work</li>
+                      <li>Punctual</li>
+                      <li>Consistant</li>
+
+                    </ul>
+                  </div>
+
+                </div>
     </div>
   </div>
   )
