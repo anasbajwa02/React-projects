b22791fd953a4e880ba59cae120c23fcae0bca91
anasbajwa02 <anas03154297874@gmail.com>
2025-07-18 20:06:25 +0500

    design theme changer ui

--- portfolio/src/components/Theme/Theme.jsx
+++ portfolio/src/components/Theme/Theme.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 import "./theme.css";
 const Theme = () => {
   return (
-    <div>Theme</div>
+    <div className="theme-container">
+      <div className="theme-title">
+        <h2>Pick Your VSCode Theme</h2>
+      </div>
+      <div className="themes">
+        <div className="vs-code">
+          <img src="/vscode.webp" alt="vs code " />
+          <h2>VS Code</h2>
+          <h4>Classic VSCode theme.</h4>
+          <button>Use Theme</button>
+        </div>
+        <div className="night-owl">
+          <img src="/nightowl.webp" alt="night owl " />
+          <h2>Night Owl</h2>
+          <h4>Vibrant and readable theme.</h4>
+          <button>Use Theme</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
